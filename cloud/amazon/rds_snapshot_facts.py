21d6e107ae1aef08d5ac6a30db0acf8164a9d355
Tom Bamford <tom@atplanet.co.za>
2015-11-29 13:23:20 +0000

    More search options for rds_snapshot_facts module
    * Filter by snapshot ID (regular expression)
    * Filter by snapshot type
    * Sort by snapshot ID or snapshot creation time
    * Sort results ascending/descending and slice results

--- cloud/amazon/rds_snapshot_facts.py
+++ cloud/amazon/rds_snapshot_facts.py
@@ -20,6 +20,8 @@ module: rds_snapshot_facts
 short_description: searches for rds snapshots to obtain the id
 description:
     - searches for rds snapshots to obtain the id
+author: "Brock Haywood (@brockhaywood), Tom Bamford (@tombamford)"
+version_added: 2.1
 options:
   snapshot_id:
     description:
@@ -33,8 +35,58 @@ options:
     description:
       - max records returned
     required: false
+  id_regex:
+    description:
+      - Filter the results by matching this regular expression against the snapshot ID.
+    default: null
+    required: false
+  snapshot_type:
+    description:
+      - Filter the results by snapshot type.
+    choices: ['automated', 'manual']
+    default: null
+    required: false
+  sort:
+    description:
+      - Optional attribute which with to sort the results.
+    choices: ['id', 'snapshot_create_time']
+    default: null
+    required: false
+  sort_order:
+    description:
+      - Order in which to sort results.
+      - Only used when the 'sort' parameter is specified.
+    choices: ['ascending', 'descending']
+    default: 'ascending'
+    required: false
+  sort_start:
+    description:
+      - Which result to start with (when sorting).
+      - Corresponds to Python slice notation.
+    default: null
+    required: false
+  sort_end:
+    description:
+      - Which result to end with (when sorting).
+      - Corresponds to Python slice notation.
+    default: null
+    required: false
+  status:
+    description:
+      - Filter the results by snapshot status.
+    choices:
+      - available
+      - backing-up
+      - creating
+      - deleted
+      - deleting
+      - failed
+      - modifying
+      - rebooting
+      - resetting-master-credentials
+    default: null
+    required: false
 
-author: "Brock Haywood (@brockhaywood)"
 extends_documentation_fragment:
     - aws
     - ec2
@@ -45,9 +97,101 @@ EXAMPLES = '''
 - local_action:
     module: rds_snapshot_facts
     snapshot_id: my-local-snapshot
+
 # Find all
 - local_action:
     module: rds_snapshot_facts
+
+# Find latest, available, automated snapshot
+- local_action:
+    module: rds_snapshot_facts
+    instance_id: my-rds-instance
+    snapshot_type: automated
+    sort: snapshot_create_time
+    sort_order: descending
+    sort_end: 1
+    status: available
+
+# Search all snapshots, filter the IDs by regular expression and sort by ID
+- local_action:
+    module: rds_snapshot_facts
+    id_regex: "^my-instance-.+$"
+    sort: id
+    sort_order: descending
+    status: available
+'''
+
+RETURN = '''
+allocated_storage:
+  description: The amount of storage allocated, in GB
+  returned: success
+  type: int
+  sample: 100
+availability_zone:
+  description: Availability zone of the source instance
+  returned: success
+  type: str
+  sample: "us-west-2b"
+engine_version:
+  description: Version of the database engine from the source instance
+  returned: success
+  type: str
+  sample: "9.3.6"
+id:
+  description: The snapshot ID
+  returned: success
+  type: str
+  sample: "rds:my-instance-2016-02-21-09-25"
+instance_create_time:
+  description: When the source instance was created
+  returned: success
+  type: str
+  sample: "2015-11-17T13:01:31.951Z"
+instance_id:
+  description: ID of the source instance
+  returned: success
+  type: str
+  sample: "my-instance"
+iops:
+  description: IOPS allocated when provisioned IOPS storage is used
+  returned: success
+  type: int
+  sample: 1500
+master_username:
+  description: Master database username
+  returned: success
+  type: str
+  sample: "mesuper"
+option_group_name:
+  description: The option group name used for the source instance
+  returned: success
+  type: str
+  sample: "default:postgres-9-3"
+port:
+  description: The listening port
+  returned: success
+  type: str
+  sample: 5432
+snapshot_create_time:
+  description: When the snapshot was initiated
+  returned: success
+  type: str
+  sample: "2014-02-28T21:28:32.000Z"
+snapshot_type:
+  description: Whether the snapshot was automated or manual
+  returned: success
+  type: str
+  sample: "automated"
+status:
+  description: The status of the snapshot
+  returned: success
+  type: str
+  sample: "available"
+vpc_id:
+  description: VPC ID of the source database
+  returned: success
+  type: str
+  sample: "vpc-4c63175a"
 '''
 
 try:
@@ -57,7 +201,9 @@ except ImportError:
     HAS_BOTO = False
 
 
-def find_snapshot_facts(module, conn, snapshot_id=None, instance_id=None, max_records=None):
+def find_snapshot_facts(module, conn, snapshot_id=None, instance_id=None, max_records=None,
+                        id_regex=None, snapshot_type=None, sort=None, sort_order=None,
+                        sort_start=None, sort_end=None, status=None):
 
     try:
         snapshots = conn.get_all_dbsnapshots(snapshot_id=snapshot_id, instance_id=instance_id, max_records=max_records)
@@ -67,22 +213,52 @@ def find_snapshot_facts(module, conn, snapshot_id=None, instance_id=None, max_re
     results = []
 
     for snapshot in snapshots:
-        results.append({
-            'engine_version': snapshot.engine_version,
+
+        data = {
             'allocated_storage': snapshot.allocated_storage,
             'availability_zone': snapshot.availability_zone,
+            'engine_version': snapshot.engine_version,
             'id': snapshot.id,
             'instance_create_time': snapshot.instance_create_time,
             'instance_id': snapshot.instance_id,
+            'iops': snapshot.iops,
             'master_username': snapshot.master_username,
-            'port': snapshot.port,
-            'status': snapshot.status,
             'option_group_name': snapshot.option_group_name,
+            'port': snapshot.port,
             'snapshot_create_time': snapshot.snapshot_create_time,
             'snapshot_type': snapshot.snapshot_type,
             'source_region': snapshot.source_region,
-            'vpc_id': snapshot.vpc_id
-        })
+            'status': snapshot.status,
+            'vpc_id': snapshot.vpc_id,
+        }
+
+        if id_regex:
+            regex = re.compile(id_regex)
+            if not regex.match(data['id']):
+                continue
+
+        if snapshot_type:
+            if data['snapshot_type'] != snapshot_type:
+                continue
+
+        if status:
+            if data['status'] != status:
+                continue
+
+        results.append(data)
+
+    if sort:
+        results.sort(key=lambda e: e[sort], reverse=(sort_order=='descending'))
+
+    try:
+        if sort and sort_start and sort_end:
+            results = results[int(sort_start):int(sort_end)]
+        elif sort and sort_start:
+            results = results[int(sort_start):]
+        elif sort and sort_end:
+            results = results[:int(sort_end)]
+    except TypeError:
+        module.fail_json(msg="Please supply numeric values for sort_start and/or sort_end")
 
     module.exit_json(results=results)
 
@@ -94,6 +270,19 @@ def main():
             snapshot_id=dict(),
             instance_id=dict(),
             max_records=dict(),
+            id_regex=dict(required=False, default=None),
+            snapshot_type=dict(required=False, default=None,
+                choices=['automated', 'manual']),
+            sort = dict(required=False, default=None,
+                choices=['id', 'snapshot_create_time']),
+            sort_order = dict(required=False, default='ascending',
+                choices=['ascending', 'descending']),
+            sort_start = dict(required=False),
+            sort_end = dict(required=False),
+            status = dict(required=False, default=None,
+                choices=['available', 'backing-up', 'creating', 'deleted',
+                         'deleting', 'failed', 'modifying', 'rebooting',
+                         'resetting-master-credentials']),
         )
     )
     module = AnsibleModule(argument_spec=argument_spec)
@@ -104,6 +293,13 @@ def main():
     snapshot_id = module.params.get('snapshot_id')
     instance_id = module.params.get('instance_id')
     max_records = module.params.get('max_records')
+    id_regex = module.params.get('id_regex')
+    snapshot_type = module.params.get('snapshot_type')
+    sort = module.params.get('sort')
+    sort_order = module.params.get('sort_order')
+    sort_start = module.params.get('sort_start')
+    sort_end = module.params.get('sort_end')
+    status = module.params.get('status')
 
     # Retrieve any AWS settings from the environment.
     region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module)
@@ -121,7 +317,14 @@ def main():
             conn=conn,
             snapshot_id=snapshot_id,
             instance_id=instance_id,
-            max_records=max_records
+            max_records=max_records,
+            id_regex=id_regex,
+            snapshot_type=snapshot_type,
+            sort=sort,
+            sort_order=sort_order,
+            sort_start=sort_start,
+            sort_end=sort_end,
+            status=status
         )
 
     except boto.exception.BotoServerError, e:
